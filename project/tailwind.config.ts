47b34ade3fae1407460c0437217b13013fea0b5d
Nouman64-cat <noumanejazghost@gmail.com>
2024-12-05 21:11:08 +0500

    coming soon

--- project/tailwind.config.ts
+++ project/tailwind.config.ts
@@ -6,11 +6,30 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // Enable class-based dark mode
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: {
+          light: "var(--background-light)", // Light mode background
+          dark: "var(--background-dark)", // Dark mode background
+        },
+        foreground: {
+          light: "var(--foreground-light)", // Light mode foreground
+          dark: "var(--foreground-dark)", // Dark mode foreground
+        },
+      },
+      fontFamily: {
+        parkinsans: ["ParkinSans", "sans-serif"],
+      },
+      keyframes: {
+        pulseGlow: {
+          '0%, 100%': { transform: 'scale(1)', boxShadow: '0 0 10px #9117E1' },
+          '50%': { transform: 'scale(1.05)', boxShadow: '0 0 20px #9117E1' },
+        },
+      },
+      animation: {
+        pulseGlow: 'pulseGlow 2s ease-in-out infinite',
       },
     },
   },
